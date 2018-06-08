d2de1c86b616c259a579fbd7a243f168059cb96a
224alpha <kirankumardas224@gmail.com>
2018-06-08 21:30:11 +0530

    sourabh's push

--- routes/admin/adminlogin.js
+++ routes/admin/adminlogin.js
@@ -4,6 +4,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var mysql = require('mysql');
+const expressValidator = require('express-validator');
 var conn = require(path.join(__dirname,'/../../dependencies/connection.js'));
 
 
@@ -20,6 +21,21 @@ router.post('/admin/login',function(req,res){
     //store the data got from frontend
     var phone = req.body.phone;
     var password = req.body.password;
+
+    //validating
+    req.checkBody('phone','Phone Number is not valid !').notEmpty().isNumber().isLength({min:10,max:10});
+    req.checkBody('password','Password is not valid !').notEmpty().isLength({min:6});
+
+    //set error variable
+    var errors = req.validationErrors();
+    if(errors){
+        console.log(errors);
+        res.send('hello error');
+     }
+     else{
+        console.log(errors);
+        res.send('no error');
+     }
 
     
 });
--- routes/user/userregister.js
+++ routes/user/userregister.js
@@ -13,6 +13,30 @@ router.get('/register', function(req, res){
 
 //store registration data in table
 router.post('/register', function(req, res){
+
+    //validation of input data
+    let f_name = req.body.first_name;
+    let l_name = req.body.last_name;
+    let email = req.body.email2;
+    let phone = req.body.phone2;
+    let dob = req.body.dob;
+    let pass = req.body.password2;
+    let pass_cnf = req.body.password2_confirm;
+
+    req.checkBody('f_name', 'First name is required').notEmpty();
+    req.checkBody('l_name', 'Last name is required').notEmpty();
+    req.checkBody('email', "E-mail can't be blank").notEmpty();
+    req.checkBody("email", "Enter a valid email address").isEmail();
+
+    var errors = req.validationErrors();
+   if(errors){
+      console.log(errors);
+      res.redirect('/');
+   }
+   else{
+      console.log('Voila');
+      res.redirect('/');
+   }
     
 })
 
