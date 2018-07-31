02dd9a9dd5370ee95ae5d11fb51d099547494c63
Deepsubhra Sarkar <deepsubhrasarkar@outlook.com>
2018-07-31 19:00:38 +0530

    homepage made

--- routes/website/store.js
+++ routes/website/store.js
@@ -2,28 +2,56 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var mysql = require('mysql');
-var conn = require(path.join(__dirname,'/../../dependencies/connection.js'));
-const fs =  require('fs');
+var conn = require(path.join(__dirname, '/../../dependencies/connection.js'));
+const fs = require('fs');
 
 
 
 
 //fetching homepage
-router.get('/',function(req,res){
-    var q1="SELECT * FROM carousel_main";
-    conn.query(q1,function(err,result){
+router.get('/', function (req, res) {
+    var q1 = "SELECT * FROM carousel_main";
+    var carouseldata, card_, tee_, choco_;
+    conn.query(q1, function (err, result) {
 
         if (err) {
             console.log(err);
         }
-        res.send(result);
-        
-        
-    });  
-    
-   
-        
-   
+        carouseldata = result;
+        var q2 = "SELECT * FROM ITEMS WHERE type_name='T-shirt'";
+        conn.query(q2, function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            tee_ = result;
+            var q3 = "SELECT * FROM ITEMS WHERE type_name='cards'";
+            conn.query(q3, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                card_ = result;
+                var q2 = "SELECT * FROM ITEMS WHERE type_name='chocolate'";
+                conn.query(q2, function (err, result) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    choco_ = result;
+                    
+                    res.render('homepage.handlebars', {
+                        nonce: req.nonce,
+                        carousel: carouseldata,
+                        tee: tee_,
+                        card: card_,
+                        choco: choco_
+                    });
+                });
+            });
+        });
+    });
+
+
+
+
 });
 
 
@@ -34,67 +62,60 @@ router.get('/',function(req,res){
 
 
 //fetch item
-router.get("/item/:itemId",function(req,res){
+router.get("/item/:itemId", function (req, res) {
     var itemId = req.params.itemId;
-    var q1 = "SELECT * FROM items WHERE id="+mysql.escape(itemId);
-    conn.query(q1,function(err,result){
-        if(err){
+    var q1 = "SELECT * FROM items WHERE id=" + mysql.escape(itemId);
+    conn.query(q1, function (err, result) {
+        if (err) {
             console.log(err);
-        }
-        else{
-            if(result.length==1){
+        } else {
+            if (result.length == 1) {
                 console.log(result);
-                var size_id=result[0].size_id;
-                var q2 = "SELECT * FROM item_category_level1 WHERE id="+mysql.escape(size_id);
-                conn.query(q2,function(err,result1){
-                    if(err){
+                var size_id = result[0].size_id;
+                var q2 = "SELECT * FROM item_category_level1 WHERE id=" + mysql.escape(size_id);
+                conn.query(q2, function (err, result1) {
+                    if (err) {
                         console.log(err);
-                    }
-                    else{
-                       if(result1.length==1){
+                    } else {
+                        if (result1.length == 1) {
                             var sizes_json = JSON.parse(result1[0].size);
-                            var sizes_array=[];
-                            var gender=[];
-                            for(var key in sizes_json){
+                            var sizes_array = [];
+                            var gender = [];
+                            for (var key in sizes_json) {
                                 sizes_array.push(sizes_json[key]);
                             }
-                            if(result[0].gender=="Both"){
-                                gender=["male","female"];
-                            }
-                            else if(result[0].gender=="Male"){
-                                gender=["male"];
-                            }
-                            else if(result[0].gender=="Female"){
-                                gender=["female"];
-                            }
-                            else{
-                                gender=[];
+                            if (result[0].gender == "Both") {
+                                gender = ["male", "female"];
+                            } else if (result[0].gender == "Male") {
+                                gender = ["male"];
+                            } else if (result[0].gender == "Female") {
+                                gender = ["female"];
+                            } else {
+                                gender = [];
                             }
 
                             var item = {
-                                id : result[0].id,
-                                name : result[0].name,
-                                price : result[0].price,
-                                sizes : sizes_array,
-                                gender : gender,
-                                type : result[0].type_name, 
-                                event : result[0].event_name             
+                                id: result[0].id,
+                                name: result[0].name,
+                                price: result[0].price,
+                                sizes: sizes_array,
+                                gender: gender,
+                                type: result[0].type_name,
+                                event: result[0].event_name
                             }
 
                             res.send(JSON.stringify(item));
-                            
-                       }
-                       else{
-                           res.redirect('/');
-                       }
+
+                        } else {
+                            res.redirect('/');
+                        }
                     }
                 });
-            }
-            else{
+            } else {
                 res.redirect("/");
             }
         }
-        
+
     });
 });
 
