2741f5d3b039876f6af4039899063f02e6aed366
224alpha <kirankumardas224@gmail.com>
2018-07-31 22:52:28 +0530

    item

--- assets/js/store/store.js
+++ assets/js/store/store.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
 if($(window).width() > 992){
     //item slides slick
     $('.homepage-item-list-1').slick({
-        infinte:true,
+        slide:'div',
+        autoplay: true,
+        prevArrow: $('.left-side2'),
+        nextArrow: $('.right-side2'),
         slidesToShow: 4,
         lidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: $('#left-side2'),
-        nextArrow: $('#right-side2'),
+        
         
     });
-    var item_section_height = $('#few_item_slider').height()/2;
+    var item_section_height = $('.few_item_slider').height()/2;
     console.log(item_section_height);
-    console.log($('#few_item_slider').height());
+    console.log($('.few_item_slider').height());
     $('.width_of_item-section').css({"top":item_section_height,"transform": "translateY(-50%)"});
 
 }
@@ -30,8 +30,8 @@ if($(window).width() <= 992 && $(window).width() > 600){
             lidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 2000,
-            prevArrow: $('#left-side2'),
-            nextArrow: $('#right-side2'),
+            prevArrow: $('.left-side2'),
+            nextArrow: $('.right-side2'),
             
         });
 
@@ -55,11 +55,26 @@ if($(window).width() <=600){
             lidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 2000,
-            prevArrow: $('#left-side2'),
-            nextArrow: $('#right-side2'),
+            prevArrow: $('.left-side2'),
+            nextArrow: $('.right-side2'),
             
         });
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
