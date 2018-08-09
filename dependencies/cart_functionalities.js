04b7d459b35081d8acb68654c0a9c103df9cb4dd
RKKUNDU <kundu.rohit06@gmail.com>
2018-08-09 22:31:00 +0530

    reviewing  & updating cart functionalities

--- dependencies/cart_functionalities.js
+++ dependencies/cart_functionalities.js
@@ -4,7 +4,7 @@ var uniqid = require('uniqid');
 var pool = require(path.join(__dirname, '/connection.js'));
 
 class items{
-    constructor(item_id,item_name,item_type,no_of_items,size,color,price,image){
+    constructor(item_id,item_name,item_type,no_of_items,size,color,price,image,cashback,delivery_charge){
         this.id=uniqid('cart-');
         this.item_id=item_id;
         this.item_name=item_name;
@@ -15,10 +15,12 @@ class items{
         this.price=price;
         this.total=parseInt(no_of_items)*parseInt(price);
         this.image=image;
+        this.cashback=cashback;
+        this.delivery_charge=delivery_charge;
     }
 }
 
-function checkExistence(req,item_id,item_name,item_type,size,color,price,image,cart_items_array)
+function checkExistence(req,item_id,item_name,item_type,size,color,price,image,cart_items_array,cashback,delivery_charge,callback)
 {
     for(var i=0;i<cart_items_array.length;i++)
     {
@@ -33,38 +35,44 @@ function checkExistence(req,item_id,item_name,item_type,size,color,price,image,c
                     var no_of_items=cart_items_array[i]["no_of_items"]+1;
                     cart_items_array[i]["no_of_items"]=no_of_items;
                     cart_items_array[i]["price"]=price;
+                    cart_items_array[i]["cashback"]=cashback;
+                    cart_items_array[i]["delivery_charge"]=delivery_charge;
+                    
                     
                     cart_items_array[i]["total"]=parseInt(no_of_items)*parseInt(price);
                     var dict={"items":cart_items_array};
 
-
-                    updateQuery(dict,req)
-                    return;
+                    updateQuery(dict,req,function(){
+                        console.log("updateQuery() callback");
+                    });
+                    return callback();
                 }
             }
         }
     };
-    add(req,item_id,item_name,item_type,size,color,price,image,cart_items_array);
+    add(req,item_id,item_name,item_type,size,color,price,image,cart_items_array,cashback,delivery_charge);
 }
 
-//item_id,no_of_items,size,color,price,image
-function  add(req,item_id,item_name,item_type,size,color,price,image,cart_items_array)
+function  add(req,item_id,item_name,item_type,size,color,price,image,cart_items_array,cashback,delivery_charge)
 {
     
     console.log("new itam added");
-    var obj=new items(item_id,item_name,item_type,1,size,color,price,image);
-var dict={"id":obj.id,"item_id":obj.item_id,"item_name":obj.item_name,"item_type":item_type,"no_of_items":obj.no_of_items,"size":obj.size,"color":obj.color,"price":obj.price,"total":obj.total,"image":obj.image};
+    var obj=new items(item_id,item_name,item_type,1,size,color,price,image,cashback,delivery_charge);
+var dict={"id":obj.id,"item_id":obj.item_id,"item_name":obj.item_name,"item_type":item_type,"no_of_items":obj.no_of_items,"size":obj.size,"color":obj.color,"price":obj.price,"total":obj.total,"image":obj.image,"cashback":cashback,"delivery_charge":delivery_charge};
     
     cart_items_array.push(dict);    
     var dict={"items":cart_items_array};
 
-    updateQuery(dict,req);
+    updateQuery(dict,req,function(){
+
+        console.log("updateQuery() callback");
+    });
                   
 }
 
 
 
-function remove(req,id,cart_items_array)
+function remove(req,id,cart_items_array,callback)
 {
     for(var i=0;i<cart_items_array.length;i++)
     {
@@ -73,13 +81,15 @@ function remove(req,id,cart_items_array)
             console.log("items removed");
             cart_items_array.splice(i,1);                
             var dict={"items":cart_items_array};
-            
-            updateQuery(dict,req);
-            return;           
+            var len=cart_items_array.length;
+            updateQuery(dict,req,function(){
+                console.log('updateQuery() callback');
+            });
+            return callback(len);           
         }       
     }
 }
-function increase_decrease(req,id,cart_items_array,value)
+function increase_decrease(req,id,cart_items_array,value,callback)
 {
     for(var i=0;i<cart_items_array.length;i++)
     {
@@ -88,23 +98,22 @@ function increase_decrease(req,id,cart_items_array,value)
             console.log("No of items increased by "+value);
             var no_of_items=cart_items_array[i]["no_of_items"];
             var total_no=parseInt(no_of_items)+parseInt(value);
-
             var total_price=0;
             if(total_no<=0)
             {
-                console.log("no of pieces is less than 0")
+                console.log("no_of_items() is less than 0.Hence removed");
                 cart_items_array.splice(i,1);
             }
             else{
-    
                 cart_items_array[i]["no_of_items"]=total_no;
                 total_price=total_no*parseInt(cart_items_array[i]["price"]);
                 cart_items_array[i]["total"]=total_price;
             }
-
             var dict={"items":cart_items_array};
-            updateQuery(dict,req);
-            return total_price;        
+            updateQuery(dict,req,function(){
+                console.log('updateQuery() callback');
+            });
+            return callback(total_price);        
         }        
     }
 }
@@ -118,41 +127,37 @@ function getTotalPrice(cart_items_array,callback)
         var x=price*no_of_items;
         sum+=x;
     }
-   // console.log("Total Price "+sum);
-     callback(sum);
+    callback(sum);
 }
 
 
-function updateQuery(dict,req)
+function updateQuery(dict,req,callback)
 {
     console.log("New Cart:\n");
     console.log(dict);
-
     pool.getConnection((err,conn)=>{
         if(err){
             console.log(err);
         }
-
         var query="UPDATE  userlist SET cart='"+JSON.stringify(dict)+"' WHERE phone='"+req.session.passport["user"]+"'";
         conn.query(query,function(err,result){
     
             if(err) throw err
-    
-            console.log("Data Updated in the cart column");
-            
+            if(result.affectedRows==1)
+            {
+                console.log("Data Updated in the cart column");
+            }
+            else{
+                console.log("Data Not updated");
+            }
         });
-
-
         conn.release();
-
 
     });
 
+    return callback();
     
 }
-
-
-
 
 exports.checkExistence=checkExistence;
 exports.add=add;
--- routes/website/cart.js
+++ routes/website/cart.js
@@ -11,69 +11,9 @@ var uniqid=require('uniqid');
 
 var  cart_functionalities= require(path.join(__dirname,'/../../dependencies/cart_functionalities.js'));
 var checkExistence=cart_functionalities.checkExistence;
-var add=cart_functionalities.add;
 var remove=cart_functionalities.remove;
 var increase_decrease=cart_functionalities.increase_decrease;
 var getTotalPrice=cart_functionalities.getTotalPrice;
-
-
-var deliveryCharge=40;
-
-
-router.post('/admin/order/placed/success/:order_id',function(req,res2){
-	console.log("web hook");
-	console.log(req.body);
-	var order_id=req.params.order_id;
-	pool.getConnection(function(err,conn){
-		if(err) console.log(err);
-	console.log(order_id);	
-		var q="SELECT * FROM temp_order WHERE id="+mysql.escape(order_id);
-console.log(q);		
-conn.query(q,function(err2,res){
-			if(err2) console.log(err2);
-		//	console.log(res);
-			console.log(res[0]);
-			if(res.length==1)
-			{
-				var user_phone=res[0].user_phone
-				var items=res[0].items;
-				var total_price=res[0].total_price;
-				var promocode=res[0].promocode;
-				var discount=res[0].discount;
-				var cashback=res[0].cashback;
-				var net_amount=res[0].net_amount;
-				var delivery_charge=res[0].delivery_charge;
-				var net_amount_with_delivery_charge =res[0].net_amount_with_delivery_charge;
-				var address=res[0].address;
-				var address_contact=res[0].address_contact;
-				var date=new Date();
-				var order_status="order placed"
-				var payment_status=req.body.status;
-				
-				var payment_id=req.body.payment_id;
-				var shorturl="haha"+req.body.shorturl;
-				var longurl=req.body.longurl;
-				var amount_paid=req.body.amount;
-				var currency=req.body.currency;
-				var instamojo_fees=req.body.fees;
-				var mac=req.body.mac;
-				var q1="INSERT INTO order_table VALUES ("+mysql.escape(order_id)+","+mysql.escape(user_phone)+","+mysql.escape(items)+","+mysql.escape(total_price)+","+mysql.escape(promocode)+","+mysql.escape(discount)+","+mysql.escape(cashback)+","+mysql.escape(net_amount)+","+mysql.escape(delivery_charge)+","+mysql.escape(net_amount_with_delivery_charge)+","+mysql.escape(address)+","+mysql.escape(address_contact)+","+mysql.escape(date)+","+mysql.escape(order_status)+","+mysql.escape(payment_status)+","+mysql.escape(payment_id)+","+mysql.escape(shorturl)+","+mysql.escape(longurl)+","+mysql.escape(amount_paid)+","+mysql.escape(currency)+","+mysql.escape(instamojo_fees)+","+mysql.escape(mac)+");";
-console.log(q1);				
-conn.query(q1,function(err3,res2)
-				{
-					if(err3) console.log(err3);
-					console.log("data inserted in order table");
-				});
-			}
-			else
-			{
-				console.log("lol lol");
-				res2.status(404);
-			}
-		
-		});
-	});
-});
 
 
 router.get('/cart',isLoggedIn,(req,res)=>{
@@ -86,18 +26,23 @@ router.get('/cart',isLoggedIn,(req,res)=>{
         conn.query(query,function(err,result){
             if(err) console.log(err);
 
-            var cart=JSON.parse(result[0]['cart']);
-    
-            console.log(cart);
-            var cart_items_array=cart["items"];
-    
-            var btnEnable=false;
-            if(cart_items_array.length>0)
+            if(result.length==1)
             {
-                btnEnable=true;
+                var cart=JSON.parse(result[0]['cart']);
+                console.log(cart);
+                var cart_items_array=cart["items"];
+                var btnEnable=false;
+                if(cart_items_array.length>0)
+                {
+                    btnEnable=true;
+                }
+                res.render('cart/cartpage.handlebars',{cart:cart_items_array,btnEnable:btnEnable});
             }
-    
-            res.render('cart/cartpage.handlebars',{cart:cart_items_array,btnEnable:btnEnable});
+            else{
+                console.log('invalid user');
+                res.redirect('/');
+            }
+            
         });
         conn.release();
     });
@@ -106,20 +51,14 @@ router.get('/cart',isLoggedIn,(req,res)=>{
 
 
 router.post('/cart/add',function(req,res){
-    console.log('i got');
-
 
 if(!req.isAuthenticated()){
     
     req.session.oldUrl="/item/"+req.body.item_id;
     console.log(req.session.oldUrl);
-    /*data={
-        redirect:'/item/'+req.body.item_id
-    }*/
     res.send("Not Logged In");
 }
 else{
-
     pool.getConnection((err,conn)=>{
         if(err){
             console.log(err);
@@ -140,127 +79,82 @@ else{
                 var item_type=result[0].type_name;
                 var imageJSON=JSON.parse(result[0].images);
                 var image=imageJSON["1"];
+
+                var cashback=result[0].cashback;
+                var delivery_charge=result[0].delivery_charge;
     
                 var q="SELECT cart FROM userlist WHERE phone="+mysql.escape(req.session.passport["user"]);
-                console.log(q);
                 conn.query(q,function(err2,result2){
     
                     if(err2) console.log(err2);
-                    //console.log(result2[0]['cart']);
-    
-                    var cart=JSON.parse(result2[0]['cart']);
-    
-                    //console.log("Cart:");
-                    //console.log(cart);
-                    
-                    var cart_items_array=cart["items"];
-                    //log(cart_items_array.length);
-    
-                //console.log("item_id: "+item_id+" no_of_items: "+no_of_items+" size: "+size+" color: "+color+" price: "+price+" image: "+image);
-    
-                checkExistence(req,item_id,item_name,item_type,size,color,price,image,cart_items_array);
-    
+                    if(result2.length==1)
+                    {
+                        var cart=JSON.parse(result2[0]['cart']);
+                        var cart_items_array=cart["items"];
+                        checkExistence(req,item_id,item_name,item_type,size,color,price,image,cart_items_array,cashback,delivery_charge,function(){
+                           console.log("checkExistence() callback"); 
+                        });
+                        res.send("done");
+                        
+                    }
+                    else{
+                        console.log('invalid user');
+                        res.send('invalid');
+                    }
                 });
             }
+            else{
+                console.log('invalid item');
+                res.send('invalid');
+            }
         });
-        res.send("hello");
-
-
         conn.release();
-
-
     });
-
 }
-    
-    
+        
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 router.post('/cart/remove',isLoggedIn,(req,res)=>{
-
 
     pool.getConnection((err,conn)=>{
         if(err){
             console.log(err);
         }
-
         var q="SELECT cart FROM userlist WHERE phone="+mysql.escape(req.session.passport["user"]);
-        console.log(q);
         conn.query(q,function(err2,result2){
     
             if(err2) console.log(err2);
     
-            var id=req.body.id;
-    
-            //console.log(result2[0]['cart']);
-    
-            var cart=JSON.parse(result2[0]['cart']);
-    
-            //console.log("Cart:");
-            //console.log(cart);
-            var cart_items_array=cart["items"];
-            //log(cart_items_array.length);
-    
-            //console.log("item_id: "+item_id+" no_of_items: "+no_of_items+" size: "+size+" color: "+color+" price: "+price+" image: "+image);
-            remove(req,id,cart_items_array);
-            res.send("remove");
-    
+            if(result2.length==1)
+            {
+                var id=req.body.id;    
+                var cart=JSON.parse(result2[0]['cart']);
+                var cart_items_array=cart["items"];
+                var len;
+                remove(req,id,cart_items_array,function(length){
+                    console.log("remove() callback");
+                    len=length;
+                });
+                res.send({done:"done",length:len});
+            }
+            else{
+                console.log('invalid user');
+                res.send({done:'invalid',length:len});
+            }
         });
-        //res.send("hello");
-        //res.redirect('/user/cart');
-
         conn.release();
-
-
-    });
-
-    
+    }); 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
 router.post('/cart/change',isLoggedIn,(req,res)=>{
-
-    console.log("change");
 
     pool.getConnection((err,conn)=>{
         if(err){
             console.log(err);
         }
         var query="SELECT * FROM items WHERE id="+mysql.escape(req.body.item_id);
-        conn.query(query,(err,res2)=>{
-            if(err) console.log(err);
-    
+        conn.query(query,(err3,res2)=>{
+            if(err3) console.log(err3);
             if(res2.length==1)
             {
                 var price=res2[0].price;
@@ -268,91 +162,64 @@ router.post('/cart/change',isLoggedIn,(req,res)=>{
                 var value=req.body.value;
     
                 var q="SELECT cart FROM userlist WHERE phone="+mysql.escape(req.session.passport["user"]);
-                console.log(q);
                 conn.query(q,function(err2,result2){
                     if(err2) console.log(err2);
-    
-                    //console.log(result2[0]['cart']);
-                    var cart=JSON.parse(result2[0]['cart']);
-    
-                    //console.log("Cart:");
-                    //console.log(cart);
-                    var cart_items_array=cart["items"];
-                    //log(cart_items_array.length);
-    
-                    //console.log("item_id: "+item_id+" no_of_items: "+no_of_items+" size: "+size+" color: "+color+" price: "+price+" image: "+image);
-                    var total_price=increase_decrease(req,id,cart_items_array,value);
-    
-                    res.send({total_price:total_price});
+                        
+                    if(result2.length==1)
+                    {
+                        var cart=JSON.parse(result2[0]['cart']);
+                        var cart_items_array=cart["items"];
+                        var total_price;
+                        increase_decrease(req,id,cart_items_array,value,function(total){
+                            console.log('increase_decrease() callback');
+                            total_price=total;
+                        });
+                        res.send({done:'done',total_price:total_price});
+                    }
+                    else{
+                        console.log('invalid user');
+                        res.send({done:'invalid'});
+                    }
+                   
                 });
+            }
+            else{
+                console.log('invalid item');
+                res.send({done:'invalid'});
             }
         });
     conn.release();    
-
-
     });
     
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 router.post('/cart/total',isLoggedIn,(req,res)=>{
-
-
     pool.getConnection((err,conn)=>{
         if(err){
             console.log(err);
         }
         var q="SELECT cart FROM userlist WHERE phone="+mysql.escape(req.session.passport["user"]);
-        console.log(q);
         conn.query(q,function(err2,result2){
             if(err2) console.log(err2);
-    
-            //console.log(result2[0]['cart']);
-            var cart=JSON.parse(result2[0]['cart']);
-    
-            //console.log("Cart:");
-            //console.log(cart);
-            var cart_items_array=cart["items"];
-            //log(cart_items_array.length);
-    
-            //console.log("item_id: "+item_id+" no_of_items: "+no_of_items+" size: "+size+" color: "+color+" price: "+price+" image: "+image);
-            var x=getTotalPrice(cart_items_array);
-    
-            console.log("total price "+x);
-              
+
+            if(result2.length==1)
+            {
+                var cart=JSON.parse(result2[0]['cart']);
+                var cart_items_array=cart["items"];
+                var x;
+                getTotalPrice(cart_items_array,function(total){
+                    x=total;
+                });
+                console.log("total price "+x);
+            }
+            else{
+                console.log('invalid user');
+                res.redirect('/');
+            }
         });
         res.send("hello");
-
         conn.release();
-
     });
-        
-   
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
