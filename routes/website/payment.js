98c7ed9c542dac552d54ec782898049e265d5d83
RKKUNDU <kundu.rohit06@gmail.com>
2018-08-11 00:18:55 +0530

    payment validation & update query

--- routes/website/payment.js
+++ routes/website/payment.js
@@ -36,18 +36,45 @@ router.get('/order/payment/success/:order_id',function(req,res){
 								if(res3[0].payment_status=='Credit')
 								{
 									console.log('payment Successful');
+									var q="SELECT * FROM order_table WHERE id="+mysql.escape(order_id);
+									conn.query(q,function(err4,res2){
+										if(err4) console.log(err4);
+										if(res2.length==1)
+										{
+											var order_status=res2[0].order_status;
+											var order_id=res2[0].order_id;
+											var payment_id=res2[0].payment_id;
+											var timestamp=res2[0].date;
+											var total=res2[0].net_amount;
+											var delivery_charge=res2[0].delivery_charge;
+											var paid=res2[0].amount_paid;
+											var items_all=JSON.parse(res2[0].items);
+											var items=items_all["items"];
+											res.render('cart/paymentsuccess',{order_status:order_status,order_id:order_id,payment_id:payment_id,timestamp:timestamp,total:total,delivery_charge:delivery_charge,paid:paid,items:items});
+										}
+										else{
+											console.log("Something is Wrong");
+											conn.release();
+											res.redirect('/');
+											return;
+										}
+									});
 								}
 								else if(res3[0].payment_status=="pending"){
 									console.log("payment is processing");
 								}
 								else{
 									console.log("Something is Wrong");
+									conn.release()
 									res.redirect('/');
+									return;
 								}
 							}
 							else{
 								console.log("Something is Wrong");
+								conn.release();
 								res.redirect('/');
+								return;
 							}
 						});
 					}
@@ -65,34 +92,12 @@ router.get('/order/payment/success/:order_id',function(req,res){
 						else{
 							console.log("Something is Wrong");
 							res.redirect('/');
+							conn.release();
 							return;
 						}
 					});
 				}
 			});
-
-			/*
-			var q="SELECT * FROM order_table WHERE payment_id="+mysql.escape(req.query.payment_id)+" && payment_request_id="+mysql.escape(req.query.payment_request_id);
-			conn.query(q,function(err2,res2){
-				if(err2) console.log(err2);
-				if(res2.length==1)
-				{
-					var order_status=res2[0].order_status;
-					var order_id=res2[0].order_id;
-					var payment_id=res2[0].payment_id;
-					var timestamp=res2[0].date;
-					var total=res2[0].net_amount;
-					var delivery_charge=res2[0].delivery_charge;
-					var paid=res2[0].amount_paid;
-					var items_all=JSON.parse(res2[0].items);
-					var items=items_all["items"];
-					res.render('cart/paymentsuccess',{order_status:order_status,order_id:order_id,payment_id:payment_id,timestamp:timestamp,total:total,delivery_charge:delivery_charge,paid:paid,items:items});
-				}
-				else{
-					res.res(404);
-				}
-			});
-			*/
 			conn.release();
 		});
 
@@ -110,7 +115,7 @@ router.post('/admin/order/placed/success/:order_id',function(req,res2){
 			console.log(res[0]);
 			if(res.length==1)
 			{
-				var user_phone=res[0].user_phone
+				var user_phone=res[0].user_phone;
 				var items=res[0].items;
 				var total_price=res[0].total_price;
 				var promocode=res[0].promocode;
@@ -124,41 +129,260 @@ router.post('/admin/order/placed/success/:order_id',function(req,res2){
 				var address=res[0].address;
 				var address_contact=res[0].address_contact;
 				var date=new Date();
-				var order_status="order placed"
-				var payment_status=req.body.status;
+
+				var order_status;
+				var payment_status;
 				
+				var payment_status_from_instamojo=res[0].payment_status_from_instamojo;
+				
+				var status=req.body.payment_status;
 				var payment_id=req.body.payment_id;
-				var shorturl="haha"+req.body.shorturl;
 				var longurl=req.body.longurl;
 				var amount_paid=req.body.amount;
-				var currency=req.body.currency;
 				var instamojo_fees=req.body.fees;
 				var mac=req.body.mac;
-				var q1="INSERT INTO order_table VALUES ("+mysql.escape(order_id)+","+mysql.escape(user_phone)+","+mysql.escape(items)+","+mysql.escape(total_price)+","+mysql.escape(promocode)+","+mysql.escape(discount)+","+mysql.escape(cashback)+","+mysql.escape(used_wallet_point)+","+mysql.escape(cashback_for_items)+","+mysql.escape(net_amount)+","+mysql.escape(delivery_charge)+","+mysql.escape(net_amount_with_delivery_charge)+","+mysql.escape(address)+","+mysql.escape(address_contact)+","+mysql.escape(date)+","+mysql.escape(order_status)+","+mysql.escape(payment_status)+","+mysql.escape(payment_id)+","+mysql.escape(shorturl)+","+mysql.escape(longurl)+","+mysql.escape(amount_paid)+","+mysql.escape(currency)+","+mysql.escape(instamojo_fees)+","+mysql.escape(mac)+");";
-				console.log(q1);				
-				conn.query(q1,function(err3,res3)
+
+				if(payment_status_from_instamojo=="Credit")
 				{
-					if(err3) console.log(err3);
-					if(res3.affectedRows==1)
+					if(status=='Credit')
 					{
-						console.log("data inserted in order table");
+						payment_status="Credit";
+						order_status='placed';
+						console.log("order placed");
+						insertIntoOrderTable(order_id,user_phone,items,total_price,promocode,discount,cashback,used_wallet_point,cashback_for_items,net_amount,delivery_charge,net_amount_with_delivery_charge,address,address_contact,date,order_status,payment_status,payment_id,longurl,amount_paid,instamojo_fees,mac,function(ans){
+							if(ans==1)
+								console.log("Data inserted in order table");
+							else{
+									console.log("Something happened wrong");
+									console.log("data not updated in order table");
+								}
+						});
+
+
 					}
 					else{
-						console.log("Something happened wrong");
-						console.log("data not updated in order table");
+						payment_status="conflict";
+						order_status='conflict';
+						console.log('order_status is in conflict');
+						updateTempOrderTable(order_id,payment_status,order_status,function(ans){
+							if(ans==1)
+								console.log("Data updated in the temp_order");
+							else{
+									console.log("Something happened wrong");
+									console.log("data not updated in temp_order table");
+							}
+						});
+
 					}
-					
-				});
+
+				}
+				//end   payment_status_from_instamojo=="Credit" case
+				else if(payment_status_from_instamojo=='"not_checked"')
+				{
+					var headers = { 'X-Api-Key': 'test_da22573aae638ce3fcb53c15f4f', 'X-Auth-Token': 'test_a0e09af12f77bfc6acded07115c'}
+					request.get(
+					'https://test.instamojo.com/api/1.1/payments/'+payment_id+"/",
+					{ headers: headers}, function(error, response, body){
+						if(!error && response.statusCode == 200)
+						{
+							var payment_details=JSON.parse(response.body);
+							var status_from_instamojo=payment_details.payment["status"];
+							if(status_from_instamojo=="Credit")
+							{
+								if(status=='Credit')
+								{
+									payment_status="Credit";
+									order_status='placed';
+									console.log("order placed");
+									insertIntoOrderTable(order_id,user_phone,items,total_price,promocode,discount,cashback,used_wallet_point,cashback_for_items,net_amount,delivery_charge,net_amount_with_delivery_charge,address,address_contact,date,order_status,payment_status,payment_id,longurl,amount_paid,instamojo_fees,mac,function(ans){
+										if(ans==1)
+											console.log("Data inserted in order table");
+										else{
+												console.log("Something happened wrong");
+												console.log("data not inserted in order table");
+											}
+									});									
+
+								}
+								else{
+									payment_status="conflict";
+									order_status='conflict';
+									console.log('order_status is in conflict');
+									updateTempOrderTable(order_id,payment_status,order_status,function(ans){
+										if(ans==1)
+											console.log("Data updated in the temp_order");
+										else{
+												console.log("Something happened wrong");
+												console.log("data not updated in temp_order table");
+										}
+									});
+								}
+
+							}
+							else{
+									if(status=='Credit')
+									{
+										payment_status="conflict";
+										order_status='conflict';
+										console.log('order_status is in conflict');
+										updateTempOrderTable(order_id,payment_status,order_status,function(ans){
+											if(ans==1)
+												console.log("Data updated in the temp_order");
+											else{
+													console.log("Something happened wrong");
+													console.log("data not updated in temp_order table");
+											}
+										});
+
+									}
+									else{
+										payment_status="failed";
+										order_status='not placed';
+										console.log('order not placed');
+										updateTempOrderTable(order_id,payment_status,order_status,function(ans){
+											if(ans==1)
+												console.log("Data updated in the temp_order");
+											else{
+													console.log("Something happened wrong");
+													console.log("data not updated in temp_order table");
+											}
+										});
+									}
+
+							}
+						}
+						//end if instamojo get request responds
+						else{
+							if(status=='Credit')
+							{
+								payment_status="conflict";
+								order_status='conflict';
+								console.log('order_status is in conflict');
+								updateTempOrderTable(order_id,payment_status,order_status,function(ans){
+									if(ans==1)
+										console.log("Data updated in the temp_order");
+									else{
+											console.log("Something happened wrong");
+											console.log("data not updated in temp_order table");
+									}
+								});
+							}
+							else{
+								payment_status="failed";
+								order_status='not placed';
+								console.log('order not placed');
+								updateTempOrderTable(order_id,payment_status,order_status,function(ans){
+									if(ans==1)
+										console.log("Data updated in the temp_order");
+									else{
+											console.log("Something happened wrong");
+											console.log("data not updated in temp_order table");
+									}
+								});
+
+							}
+
+						}
+						//end if instamojo get request does not responds
+					});
+			
+
+				}
+				//end   payment_status_from_instamojo=="not_checked" case
+				else{
+
+				}
+
 			}
+			//end order details found
 			else
 			{
 				res2.status(404);
 			}
+			//end order_details not found
 		});
+		//end get order details from temp_order
 		conn.release();
 	});
+	//end pool
 });
 
 
+function insertIntoOrderTable(order_id,user_phone,items,total_price,promocode,discount,cashback,used_wallet_point,cashback_for_items,net_amount,delivery_charge,net_amount_with_delivery_charge,address,address_contact,date,order_status,payment_status,payment_id,longurl,amount_paid,instamojo_fees,mac,callback)								
+{
+	pool.getConnection(function(err,conn){
+		var q1="INSERT INTO order_table VALUES ("+mysql.escape(order_id)+","+mysql.escape(user_phone)+","+mysql.escape(items)+","+mysql.escape(total_price)+","+mysql.escape(promocode)+","+mysql.escape(discount)+","+mysql.escape(cashback)+","+mysql.escape(used_wallet_point)+","+mysql.escape(cashback_for_items)+","+mysql.escape(net_amount)+","+mysql.escape(delivery_charge)+","+mysql.escape(net_amount_with_delivery_charge)+","+mysql.escape(address)+","+mysql.escape(address_contact)+","+mysql.escape(date)+","+mysql.escape(order_status)+","+mysql.escape(payment_status)+","+mysql.escape(payment_id)+","+mysql.escape(longurl)+","+mysql.escape(amount_paid)+","+mysql.escape(instamojo_fees)+","+mysql.escape(mac)+");";
+		console.log(q1);				
+		conn.query(q1,function(err3,res3)
+		{
+			if(err3) console.log(err3);
+			if(res3.affectedRows==1)
+			{
+				deleteRowFromTempOrderTable(order_id,function(ans){
+					if(ans==1)
+					{
+						console.log("row deleted from temp_order");
+					}
+					else{
+						console.log("row was not deleted from temp_order");
+					}
+					pool.release();
+					return callback(1);
+				});
+				
+			}
+			else{
+				pool.release();
+				return callback(0);
+				
+			}
+			
+		});
+		
+	});
+}
+
+function updateTempOrderTable(order_id,payment_status,order_status,callback)
+{
+	pool.getConnection(function(err,conn){
+		var q1="UPDATE temp_order SET payment_status="+mysql.escape(payment_status)+",order_status="+mysql.escape(order_status)+" WHERE id="+mysql.escape(order_id);		
+		console.log(q1);				
+		conn.query(q1,function(err3,res3)
+		{
+			if(err3) console.log(err3);
+			if(res3.affectedRows==1)
+			{
+				pool.release();
+				return callback(1);
+			}
+			else{
+				pool.release();
+				return callback(0);	
+			}
+		});
+		
+	});
+}
+
+function deleteRowFromTempOrderTable(order_id,callback){
+	pool.getConnection(function(err,conn){
+		var q1="DELETE FROM temp_order WHERE id="+mysql.escape(order_id);		
+		console.log(q1);				
+		conn.query(q1,function(err3,res3)
+		{
+			if(err3) console.log(err3);
+			if(res3.affectedRows==1)
+			{
+				pool.release();
+				return callback(1);
+			}
+			else{
+				pool.release();
+				return callback(0);	
+			}
+		});
+		
+	});
+}
 
 module.exports=router;
